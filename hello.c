05462ea3c2ce471b1a61d376435a06cb11da6421
mingikim1998 <babel5165@gmail.com>
2020-09-13 15:04:16 +0900

    C

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("hello world\n");
+	printf("Hello, %s\n","world\n");
+	return 0; // return 0의 의미는 성공적으로 함수를 마쳤다
+
+	if (a > 10)
+	{
+		printf("a");
+	}
+	
+	for (int i = 0; i < 10; i++)
+	{
+		printf("Hello, world!\n");
+	}
+}
+
