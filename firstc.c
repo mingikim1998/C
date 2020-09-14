c5a5fbc802e89d13a03c0736098490cedcfc62be
mingikim1998 <babel5165@gmail.com>
2020-09-14 23:57:51 +0900

    slowwwwwww

--- firstc.c
+++ firstc.c
@@ -2,5 +2,17 @@
 
 int main(){
 	printf("동해물과 백두산이\n마르고\n닳도록\n");
+	// 1 ~ 7 강
+	// int: 32비트(4바이트), 정수를 담는 데 쓰임
+	int a = 5;
+	int b = 3;
+
+	int hap = a + b;
+	printf("%d + %d = %d\n", a, b, hap);
+
+	int nameoji = a % b;
+
+	printf("%d %% %d = %d\n", a, b, nameoji);
+
 }
 
